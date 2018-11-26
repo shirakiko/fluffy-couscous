33de6feaac997aa7b3e69b6d42191b9c55ce477d
shirakiko <3036797674@qq.com>
2018-11-26 16:39:52 +0800

    new one

--- 我的计算器.py
+++ 我的计算器.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 win = tk.Tk()
 win.title('Calculator')
-
+"""设置窗口"""
 def fun1(p):   
     ent.insert(tk.INSERT,p)
 """完成所有用按钮输入"""    
@@ -24,40 +24,40 @@ led=tk.StringVar()
 ent= tk.Entry(win,textvariable=led)
 ent.grid(row =0, columnspan=3)
 
-but1 = tk.Button(win,text='1',command=lambda : fun1("1"))
+but1 = tk.Button(win,text='1',width=5,command=lambda : fun1("1"))
 but1.grid(row =1,column = 0)
 
-but2 = tk.Button(win,text ='2',command=lambda:fun1("2"))
+but2 = tk.Button(win,text ='2',width=5,command=lambda:fun1("2"))
 but2.grid(row =1,column = 1)
 
-but3 = tk.Button(win,text ='3',command=lambda:fun1("3"))
+but3 = tk.Button(win,text ='3',width=5,command=lambda:fun1("3"))
 but3.grid(row =1,column = 2)
 
-but4 = tk.Button(win,text ='4',command=lambda:fun1("4"))
+but4 = tk.Button(win,text ='4',width=5,command=lambda:fun1("4"))
 but4.grid(row =2,column = 0)
 
-but5 = tk.Button(win,text ='5',command=lambda:fun1("5"))
+but5 = tk.Button(win,text ='5',width=5,command=lambda:fun1("5"))
 but5.grid(row =2,column = 1)
 
-but6 = tk.Button(win,text ='6',command=lambda:fun1("6"))
+but6 = tk.Button(win,text ='6',width=5,command=lambda:fun1("6"))
 but6.grid(row =2,column = 2)
 
-but7 = tk.Button(win,text ='7',command=lambda:fun1("7"))
+but7 = tk.Button(win,text ='7',width=5,command=lambda:fun1("7"))
 but7.grid(row =3,column = 0)
 
-but8 = tk.Button(win,text ='8',command=lambda:fun1("8"))
+but8 = tk.Button(win,text ='8',width=5,command=lambda:fun1("8"))
 but8.grid(row =3,column = 1)
 
-but9 = tk.Button(win,text ='9',command=lambda:fun1("9"))
+but9 = tk.Button(win,text ='9',width=5,command=lambda:fun1("9"))
 but9.grid(row =3,column = 2)
 
-but0 = tk.Button(win,text ='0',command=lambda:fun1("0"))
+but0 = tk.Button(win,text ='0',width=5,command=lambda:fun1("0"))
 but0.grid(row =4,column = 1)
 
-but_eql = tk.Button(win,text ='=',command=cal)
+but_eql = tk.Button(win,text ='=',width=5,command=cal)
 but_eql.grid(row =4,column = 2)
 
-butCls = tk.Button(win,text ='cls',command=cls)
+butCls = tk.Button(win,text ='cls',width=5,command=cls)
 butCls.grid(row=4,column=0)
 
 but_add =tk.Button(win,text='+',command=lambda: fun1('+'))
@@ -78,6 +78,8 @@ but_dot.grid(row=6,column=2)
 but_del =tk.Button(win,text="delete",command=dele)
 but_del.grid(row=6,column=1)
 """设置各个按钮"""
+
 win.mainloop()
+"""程序循环运行"""
 
 #报告发送到315350457@qq.com
