7763e44d408bafb884a987422844a5c8f2e28c95
shirakiko <3036797674@qq.com>
2018-12-02 01:33:37 +0800

    final?

--- Hit_Boats.py
+++ Hit_Boats.py
@@ -1,19 +1,17 @@
 board = [['1','2','3'],['4','5','6'],['7','8','9']]
-
+#创建一个3x3的坐标格
 y= int(input("Please enter the 'y'"))
 x= int(input("Please enter the 'x'"))
 board[2][2]="x"
+#设定一个战舰的位置
 
-def hit(y,x):
-    if board[y][x] == "x":
-        print("Hit!")
-        board[y][x] = "0"
-    else:
-        print("Please try again!")
-        y= int(input("Please enter the 'y'"))
-        x= int(input("Please enter the 'x'"))
-        while board[y][x]!="0":
-            hit(y,x)
+while board[y][x]!="x":
+    print("Please try again!")
+    y= int(input("Please enter the 'y'"))
+    x= int(input("Please enter the 'x'"))
+else:
+    print("Hit!")   
+                      
+       
+#报出坐标击沉之
 
-     
-hit(y,x)
