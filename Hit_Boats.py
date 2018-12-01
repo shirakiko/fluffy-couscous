2039fcbcd1d55d2b4f61c53fb24dbf69c509f6db
shirakiko <3036797674@qq.com>
2018-12-02 00:22:43 +0800

    improve

--- Hit_Boats.py
+++ Hit_Boats.py
@@ -1,8 +1,8 @@
 board = [['1','2','3'],['4','5','6'],['7','8','9']]
-print(board[0])
-print(board[2][1])
+
 y= int(input("Please enter the 'y'"))
 x= int(input("Please enter the 'x'"))
+board[2][2]="x"
 
 def hit(y,x):
     if board[y][x] == "x":
@@ -12,5 +12,8 @@ def hit(y,x):
         print("Please try again!")
         y= int(input("Please enter the 'y'"))
         x= int(input("Please enter the 'x'"))
-while board[y][x]!="0":
-    hit(y,x)
+        while board[y][x]!="0":
+            hit(y,x)
+
+     
+hit(y,x)
