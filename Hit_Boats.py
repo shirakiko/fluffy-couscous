a3fdd0c9399d99761da2f2c7b86adc833ad9dfc8
shirakiko <3036797674@qq.com>
2018-12-03 20:29:55 +0800

    develop

--- Hit_Boats.py
+++ Hit_Boats.py
@@ -1,11 +1,16 @@
 board = [['1','2','3'],['4','5','6'],['7','8','9']]
 #创建一个3x3的坐标格
-y= int(input("Please enter the 'y'"))
-x= int(input("Please enter the 'x'"))
-board[2][2]="x"
+
+a= int(input("Please set a coordinate for your boat."))
+b= int(input("Please set the other one for it."))
+board[a-1][b-1]="x"
 #设定一个战舰的位置
 
-while board[y][x]!="x":
+("Now you can launch a missile to hit your enermy.")
+y= int(input("Please enter the 'y'"))
+x= int(input("Please enter the 'x'"))
+
+while board[y-1][x-1]!="x":
     print("Please try again!")
     y= int(input("Please enter the 'y'"))
     x= int(input("Please enter the 'x'"))
