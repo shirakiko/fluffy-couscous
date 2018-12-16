6a149e7bdbcbba563cdb48337ba0cba88379f412
shirakiko <3036797674@qq.com>
2018-12-16 19:37:03 +0800

    add Button

--- Alien_invading/alien_invasion.py
+++ Alien_invading/alien_invasion.py
@@ -6,6 +6,7 @@ from ship import Ship
 from pygame.sprite import Group
 from alien import Alien
 from game_stats import GameStats
+from button import Button
 
 def run_game():
     #初始化pygame、设置和屏幕对象
@@ -14,6 +15,8 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height)) 
    
     pygame.display.set_caption("Alien Invasion")
+    #创建Play按键
+    play_button = Button(ai_settings,screen,"Play")
     #创建一个用于存储游戏统计信息的实例
     stats = GameStats(ai_settings)
     #创建一艘飞船
@@ -36,7 +39,7 @@ def run_game():
             gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
             gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
             
-        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
         clock.tick(250)
        
         
--- Alien_invading/game_functions.py
+++ Alien_invading/game_functions.py
@@ -9,7 +9,7 @@ def check_events():
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-def update_screen(ai_settings,screen,ship,aliens,bullets):
+def update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button):
     """更新屏幕上的图像，并切换到新屏幕"""
     #每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -19,6 +19,10 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+    #如果游戏处于非活动状态,就绘制Play按钮
+    if not stats.game_active:
+        play_button.draw_button()
+        
     #让最近绘制的屏幕可见
     pygame.display.flip()
 
