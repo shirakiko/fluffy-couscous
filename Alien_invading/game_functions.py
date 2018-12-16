8d358cdd06d15da0db54fbf44cc44ab321b60ca9
shirakiko <3036797674@qq.com>
2018-12-16 19:03:35 +0800

    developing

--- Alien_invading/game_functions.py
+++ Alien_invading/game_functions.py
@@ -133,18 +133,31 @@ def update_aliens(ai_settings,stats,screen,ship,aliens,bullets):
     if pygame.sprite.spritecollideany(ship,aliens):
         print("Ship hit!!")
         ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+    #检查是否有外星人到达了屏幕底端
+    check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets)
 def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
-    """响应被外星人撞到飞船"""
-    #将ship_left减1
-    stats.ships_left -= 1
+    """响应被外星人撞到的飞船"""
+    if stats.ships_left >0:
+        #将ship_left减1
+        stats.ships_left -= 1
 
-    #清空外星人列表和子弹列表
-    aliens.empty()
-    bullets.empty()
+        #清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty()
 
-    #创建一群新的外星人，并将飞船放到屏幕底端中央
-    create_fleet(ai_settings,screen,ship,aliens)
-    ship.center_ship()
+        #创建一群新的外星人，并将飞船放到屏幕底端中央
+        create_fleet(ai_settings,screen,ship,aliens)
+        ship.center_ship()
 
-    #暂停
-    sleep(0.5)
+        #暂停
+        sleep(0.5)
+    else:
+        stats.game_active = False
+def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
+    """检查是否有外星人到达了屏幕底部"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            #像飞船被撞到一样进行处理
+            ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+            break
