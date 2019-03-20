f817e13a2c1873ae48047def92fe2d039655a8f6
shirakiko <3036797674@qq.com>
2019-03-20 15:32:13 +0800

    demo

--- divination/Get.py
+++
@@ -1,7 +0,0 @@
-def get_hexagram():
-    import random
-    list1 = []
-    for i in range(6):
-        a = random.randint(0, 1)
-        list.append(list1, a)
-
---
+++ divination/get_hexagram.py
@@ -0,0 +1,6 @@
+import random
+
+
+def get_hexagram(list_need):
+    for i in range(6):
+        list.append(list_need, random.randint(0, 1))
